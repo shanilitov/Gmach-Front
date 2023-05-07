6e1c2c48356260fe83cfca10af440ee383752804
shanilitov <116593341+shanilitov@users.noreply.github.com>
2023-05-07 13:35:40 +0300

    Working on the Router

--- gmach_front/src/App.js
+++ gmach_front/src/App.js
@@ -5,15 +5,23 @@ import Asynchronous from './Components/JavaScriptFiles/AsynchronousFeild';
 import BasicTextFields from "./Components/JavaScriptFiles/BasicTextFields";
 import LogIn from './Components/JavaScriptFiles/LogIn';
 import NewUser from './Components/JavaScriptFiles/NewUser';
+import { Route } from '@mui/icons-material';
+import { Router } from '@mui/icons-material';
 
 
 export default function App() {
   return (
-    <div >
-      <LogIn/>
-      <NewUser/>
+    <div>
+      <Router>
+        <Route path="/">
+          <Route path="/logIn" element={<LogIn/>}/>
+          <Route path="/signIn" element={<NewUser/>}/>
+        </Route>
+      </Router>
     </div>
   );
+
 }
+
 
 
