ed0b15bc51d6ef738484e5c43e4cc09b1e5ae898
shani litoff <shanilitov@gmail.com>
2023-09-03 17:14:12 +0300

    Add tasks to the project

--- gmach_front/src/Components/JavaScriptFiles/MaimComponent/Deposits.js
+++ gmach_front/src/Components/JavaScriptFiles/MaimComponent/Deposits.js
@@ -1,23 +1,25 @@
 import AlignItemsList from "../HelpingComponent/AlignItemsList"
 
-export default function Deposits(props){
-    let type= props.type
-    let deposits= props.items
-    return(
-        <div>
-    <h2
-          style={{
-            color: "rgb(0,32,96)",
-            gridColumn: "2/span 3",
-            gridRow: "1",
-          }}
-        >
-          השקעות
-        </h2>
-        <AlignItemsList/>
-        <AlignItemsList/>
-        <AlignItemsList/>
-        <AlignItemsList/>
-        </div>
-    )
+export default function Deposits(props) {
+  // TODO: ask from the server for the deposit of the client, and display them in the current template.
+  // TODO: add option to open a new investment.
+  let type = props.type
+  let deposits = props.items
+  return (
+    <div>
+      <h2
+        style={{
+          color: "rgb(0,32,96)",
+          gridColumn: "2/span 3",
+          gridRow: "1",
+        }}
+      >
+        השקעות
+      </h2>
+      <AlignItemsList />
+      <AlignItemsList />
+      <AlignItemsList />
+      <AlignItemsList />
+    </div>
+  )
 }
--- gmach_front/src/Components/JavaScriptFiles/MaimComponent/Loans.js
+++ gmach_front/src/Components/JavaScriptFiles/MaimComponent/Loans.js
@@ -1,20 +1,24 @@
 import AlignItemsList from "../HelpingComponent/AlignItemsList"
 
-export default function Loans(props){
-   let type= props.type
-    let Loans= props.items
-    return(
-        <div>
-            <h2
-          style={{
-            color: "rgb(0,32,96)",
-            gridColumn: "1/span 2",
-            gridRow: "1",
-          }}
-        >
-          הלוואות
-        </h2>
-        <AlignItemsList></AlignItemsList>
-        </div>
-    )
+export default function Loans(props) {
+  // TODO: Ask from the server for the client real loans, and change the displaytion to them.
+  // TODO: change the addLoan to a nicer view.
+  // TODO: add option to see the state of loan application.
+  let type = props.type
+  let Loans = props.items
+  return (
+    <div>
+      <h2
+        style={{
+          color: "rgb(0,32,96)",
+          gridColumn: "1/span 2",
+          gridRow: "1",
+        }}
+      >
+        הלוואות
+      </h2>
+      <AlignItemsList></AlignItemsList>
+      <a href="AddLoan">Add Loan</a>
+    </div>
+  )
 }
