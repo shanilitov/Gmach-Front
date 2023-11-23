3ba606d61968881a4566e96514e4f50749e0c9f5
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-11-23 17:26:43 +0200

    Add contant to Contact.js file. Not finished.

--- gmach_front/src/Components/JavaScriptFiles/HelpingComponent/Contact.js
+++ gmach_front/src/Components/JavaScriptFiles/HelpingComponent/Contact.js
@@ -1,25 +1,29 @@
 
 import React from 'react';
 import Bar from './Bar';
+import "../../../CSSFiles/StylePage.css"
 import Toolbar from '@mui/material/Toolbar';
 import Link from '@mui/material/Link';
+import ContactField from './ContactField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import  { useState } from 'react';
 
-
-function Contact(){
+function Contact() {
     const sections = [
-        { title: 'About us', url: '/AboutUs' },//Blog1- talking about the company.
-        { title: 'Activity', url: '/Graphes' }, //Grafes- show the activity in company in grafs.
-        { title: 'Searches', url: '/Searches' }, //Blog2- talking about searches in economy.
-        { title: 'Our services', url: '/Services' },//Blog3- talking about the services that we give.
-        { title: 'Contact us', url: '/ContactUs' },//Blog4- details how to contact us.
-        { title: 'Articles', url: '/Articles' },//Articles that talking about economy etc.
+        { title: 'About us', url: '/AboutUs' },
+        { title: 'Activity', url: '/Graphes' },
+        { title: 'Searches', url: '/Searches' },
+        { title: 'Our services', url: '/Services' },
+        { title: 'Contact us', url: '/ContactUs' },
+        { title: 'Articles', url: '/Articles' },
     ];
-
+    const [content, setContent] = useState('fields');
 
     return (
         <div>
             <Bar />
-            <div style={{ zIndex: "99", height: "5%", backgroundColor:"rgba(0, 32, 96, 0.5)", marginTop: "9%", color: "rgb(223, 221, 53)", position: "fixed", width: "100%", padding: "1%"}}>
+            <div style={{ zIndex: "99", height: "5%", marginTop: "10%", backgroundColor: "rgba(0, 32, 96, 0.5)", color: "rgb(223, 221, 53)", position: "fixed", width: "100%", padding: "1%" }}>
                 <Toolbar
                     component="nav"
                     variant="dense"
@@ -39,10 +43,39 @@ function Contact(){
                     ))}
                 </Toolbar>
             </div>
-            <h1>Contact Component</h1>
-            {/* Add your contact form or any other content here */}
+        <div style={{ marginTop: "15%", display: "inline-block", width: "50%", marginLeft: "25%" }}>
+            <h1>Contact us</h1>
+            {(content === 'fields') ? (
+                <div className='contactFields'>
+                    <ContactField text="Name" type="text" icon="AccountCircle" />
+                    <ContactField text="Email" type="email" icon="DraftsIcon" />
+                    <ContactField text="Phone" type="tel" icon="PhoneIcon" />
+                    <ContactField text="Message" type="text" icon="EditNoteIcon" />
+                    <div className='contactFields'>
+                        <Button variant="outlined" endIcon={<SendIcon />} onClick={()=>{setContent('send')}}>
+                            Send
+                        </Button>
+                    </div>
+
+                </div>
+            ) : (
+                <div className='contentSend'>
+                    <h1>Thank you for contacting us!</h1>
+                    <h2>We will contact you as soon as possible.</h2>
+                </div>
+            )}
+           <h2>Here you can find all the information about our company.</h2>
+            <h3>Address: 1st Street, New York City, USA</h3>
+            <h3>Phone: 123456789</h3>
+            <h3>Email: library.project.me@gmail.com </h3>
+            <h3>Facebook: https://www.facebook.com/library.project.me</h3>
+            <h3>Twitter: https://twitter.com/library.project.me</h3>
+            <h3>Instagram: https://www.instagram.com/library.project.me</h3>
+        </div>
         </div>
     );
-};
+}
 
 export default Contact;
+
+
---
+++ gmach_front/src/Components/JavaScriptFiles/HelpingComponent/ContactField.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import PhoneIcon from '@mui/icons-material/Phone';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+
+
+
+export default function contactField(props) {
+    const text = props.text;
+    const type = props.type;
+    const icon = props.icon;
+    return (
+        <div>
+            <div className='contactFields'>
+                <FormControl variant="standard">
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                        {text}
+                    </InputLabel>
+                    <Input
+                        type={type}
+                        id=""
+                        className={icon == "EditNoteIcon"? 'EditNoteField' : ''}
+                        startAdornment={
+                            <InputAdornment position="start" color="rgb(0, 32, 96)">
+                                {(icon == "AccountCircle") ? <AccountCircle sx={{ color: "rgb(223, 221, 53)" }} /> :
+                                    (icon == 'DraftsIcon') ? <DraftsIcon sx={{ color: "rgb(223, 221, 53)" }} /> :
+                                    (icon == "EditNoteIcon")?<EditNoteIcon sx={{ color: "rgb(223, 221, 53)" }}/>: <PhoneIcon sx={{ color: "rgb(223, 221, 53)" }} />
+
+                                }
+                            </InputAdornment>
+                        }
+                            />
+                            </FormControl>
+            </div>
+        </div>
+    )
+}
