736f52ff9a055400394d355093ab186ca3efa8f0
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-11-28 10:59:58 +0200

    Fix merge mistakes

--- gmach_front/src/Components/JavaScriptFiles/MaimComponent/NewUser.js
+++ gmach_front/src/Components/JavaScriptFiles/MaimComponent/NewUser.js
@@ -10,17 +10,41 @@ import { Password } from "@mui/icons-material";
 import ErrorAlert from "../HelpingComponent/ErrorAlert";
 import logoPhoto from "../../../img/logoPhoto.png";
 import CircularIntegration from "../HelpingComponent/CircularIntegration";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 
 function NewUser() {
-  //TODO: add function that sign in the user!
+
+  //Navigate in case user register
+  const navigate = useNavigate();
+  const NavigateFunc = (data) => {
+    navigate('/App', { state: { props: data } })
+  }
+
+  //chack if the values are good filled
+  function isInputValid() {
+    return true; // just till we finish
+    if (password.length < 8) {
+      setMessage("Password must contain at least 8 characters.");
+      return false;
+    }
+    if (phone.length !== 10) {
+      setMessage("Not a valid phone number.");
+      return false;
+    }
+    if (!email.includes("@gmail.com")) {
+      setMessage("Not a valid email address.");
+      return false;
+    }
+    return true;
+  }
+
+
+
 
   let alert = false;
-  //let message = ""
-  /*let message = {
-    text: "Sorry, some details are missing. Please fill in all fields.",
-  };*/
 
   const [showAlert, setShowAlert] = useState(false);
   const [message, setMessage] = useState("");
@@ -33,6 +57,8 @@ function NewUser() {
   const [validPassword, setValidPassword] = useState("");
 
   function checkNewUser() {
+
+    //help consols:
     console.log("userName: " + userName);
     console.log("phone: " + phone);
     console.log("userId: " + userId);
@@ -40,41 +66,8 @@ function NewUser() {
     console.log("email: " + email);
     console.log("password:" + password);
     console.log("validPassword: " + validPassword);
-    if (
-      (userName !== "") &
-      (userId !== "") &
-      (address !== "") &
-      (phone !== "") &
-      (email !== "") &
-      (password !== "") &
-      (validPassword !== "")
-    ) {
-      console.log("All field are different of ''");
-      if (password != validPassword) {
-        console.log("Sorry, Password fields doesn't match.");
-        setMessage("Sorry, Password fields doesn't match.");
-        console.log("message.text: " + message);
-        setShowAlert(true);
-      } else {
-        if (password.length < 8) {
-          setMessage("Password must contains at least 8 chars.");
-          console.log("Password must contains at least 8 chars.");
-          console.log("message.text: " + message);
-          setShowAlert(true);
-        }
-        if (phone.length != 10) {
-          console.log("Not valid phone number.");
-          setMessage("Not valid phone number.");
-          setShowAlert(true);
-          console.log("message.text: " + message);
-        }
-        if (!email.includes("@gmail.com")) {
-          console.log("Not valid email address.");
-          setMessage("Not valid email address.");
-          setShowAlert(true);
-        }
-      }
 
+    if (isInputValid()) {
       //This object used to register user
       const NewUser = {
         UserEmail: email,
@@ -90,7 +83,6 @@ function NewUser() {
         Password: password,
       };
 
-      //let URL  ="https://localhost:7275"
       let URL = "https://localhost:7275/api/User/SignIn";
       let data = NewUser;
 
@@ -105,39 +97,34 @@ function NewUser() {
       console.log(showAlert);
       if (showAlert != true) {
         console.log("Before fetch")
-        fetch(URL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ...NewUser, // spread the existing fields
-            // add the new fields                                   
-            /* UserEmail: email,
-             UserPhone: phone,
-             UserAddress: address,
-             UserIdentityNumber: userId,
-             UserName: userName,
-             UserPassword: password
-            */ //Don't need to add this fields because they already exist in NewUser object.
-          }),
-        })
-          .then((response) => response.json())
-          .then((answer) => {
-            console.log(answer);
-            console.log("Fetch occurred... answer is: " + answer);
-            // handle the response from the server
-            if (answer === "OK") {
-              console.log("User already exists!");
-            } else {
-              URL = "https://localhost:7275/api/User/SignIn";
-              data = NewUser;
-              fetch(URL, option)
-                .then((response) => response.json())
-                .then((ans) => {
-                  if (ans === "OK") console.log("User registered!");
-                  //Finish!!
-                })
+        try {
+          fetch(URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              ...NewUser, // spread the existing fields
+              //Don't need to add this fields because they already exist in NewUser object.
+            }),
+          })
+            .then((response) => {
+              response.json().then((data) => {
+                console.log("Server responsed!! Data: " + JSON.stringify(data));
+                if (data.userId === 0) {
+                  console.log("User Exist!")
+                }
+                else {
+                  if (data.userId < 0) {
+                    console.log("Error in server!")
+                  }
+                  else {
+                    NavigateFunc(data)
+                  }
+
+                }
+              })
+
                 .catch((error) => {
                   console.error('Error:', error);
                 })
@@ -157,81 +144,86 @@ function NewUser() {
         setShowAlert(true);
       }
     }
+    else {
+      setShowAlert(true);
+    }
+  }
 
 
-    return (
-      <div>
-        <div className="">
-          Sara
-          <img src={logoPhoto} alt="Logo" className="photo" />
-          <h1 id="h_newUser" color="blue" >הרשמה</h1><div className="back">
-            <div className="NewUserFeilds" onBlur={() => setShowAlert(false)}>
-              <BasicTextFields
-                header="שם"
-                type="text"
-                func={(ev) => setUserName(ev.target.value)}
-                onBlur={() => setShowAlert(false)}
-                required
-              />
-              <BasicTextFields
-                header="תעודת זהות"
-                type="number"
-                id="idField"
-                func={(ev) => setUserId(ev.target.value)}
-                onBlur={() => setShowAlert(false)}
-                required
-              />
-              <BasicTextFields
-                header="כתובת"
-                type="text"
-                func={(ev) => setAddress(ev.target.value)}
-                onBlur={() => setShowAlert(false)}
-                required
-              />
-              <BasicTextFields
-                header="טלפון"
-                type="number"
-                id="phoneField"
-                onBlur={() => setShowAlert(false)}
-                func={(ev) => setPhone(ev.target.value)}
-                required
-              />
-              <BasicTextFields
-                header="כתובת מייל"
-                type="email"
-                id="emailField"
-                onBlur={() => setShowAlert(false)}
-                func={(ev) => setEmail(ev.target.value)}
-                required
-              />
-              <BasicTextFields
-                header="צור סיסמא"
-                type="password"
-                id="passwordField"
-                onBlur={() => setShowAlert(false)}
-                func={(ev) => setPassword(ev.target.value)}
-                required
-              />
-              <BasicTextFields
-                header="חזור על הסיסמא"
-                type="password"
-                id="validField"
-                onBlur={() => setShowAlert(false)}
-                func={(ev) => setValidPassword(ev.target.value)}
-                required
-              />
-            </div>
-            <div id="NewUserBtn" /*onClick={checkNewUser}*/>
-              <BasicButtons value="הרשם" function={checkNewUser} />
-              {showAlert === true ? <ErrorAlert msg={message} /> : <></>}
-            </div>
+
+  return (
+    <div>
+      <div className="">
+        Sara
+        <img src={logoPhoto} alt="Logo" className="photo" />
+        <h1 id="h_newUser" color="blue" >הרשמה</h1><div className="back">
+          <div className="NewUserFeilds" onBlur={() => setShowAlert(false)}>
+            <BasicTextFields
+              header="שם"
+              type="text"
+              func={(ev) => setUserName(ev.target.value)}
+              onBlur={() => setShowAlert(false)}
+              required
+            />
+            <BasicTextFields
+              header="תעודת זהות"
+              type="number"
+              id="idField"
+              func={(ev) => setUserId(ev.target.value)}
+              onBlur={() => setShowAlert(false)}
+              required
+            />
+            <BasicTextFields
+              header="כתובת"
+              type="text"
+              func={(ev) => setAddress(ev.target.value)}
+              onBlur={() => setShowAlert(false)}
+              required
+            />
+            <BasicTextFields
+              header="טלפון"
+              type="number"
+              id="phoneField"
+              onBlur={() => setShowAlert(false)}
+              func={(ev) => setPhone(ev.target.value)}
+              required
+            />
+            <BasicTextFields
+              header="כתובת מייל"
+              type="email"
+              id="emailField"
+              onBlur={() => setShowAlert(false)}
+              func={(ev) => setEmail(ev.target.value)}
+              required
+            />
+            <BasicTextFields
+              header="צור סיסמא"
+              type="password"
+              id="passwordField"
+              onBlur={() => setShowAlert(false)}
+              func={(ev) => setPassword(ev.target.value)}
+              required
+            />
+            <BasicTextFields
+              header="חזור על הסיסמא"
+              type="password"
+              id="validField"
+              onBlur={() => setShowAlert(false)}
+              func={(ev) => setValidPassword(ev.target.value)}
+              required
+            />
+          </div>
+          <div id="NewUserBtn" /*onClick={checkNewUser}*/>
+            <BasicButtons value="הרשם" function={checkNewUser} />
+            {showAlert === true ? <ErrorAlert msg={message} /> : <></>}
           </div>
         </div>
-
       </div>
-    );
-  }
+
+    </div>
+  );
 }
+
 export default NewUser;
 //TODO: Check user input. In ID field- after each number check that user entered a number and not a letter. SARA.
 
