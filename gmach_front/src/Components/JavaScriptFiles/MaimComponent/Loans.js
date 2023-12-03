7da2b018a24e3a8337f27f98f433990b8f48d7cc
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-12-03 18:10:37 +0200

    Mow, "No loans" message is display.

--- gmach_front/src/Components/JavaScriptFiles/MaimComponent/Loans.js
+++ gmach_front/src/Components/JavaScriptFiles/MaimComponent/Loans.js
@@ -9,33 +9,39 @@ export default function Loans(props) {
   // TODO: Ask from the server for the client real loans, and change the displaytion to them.
   // TODO: change the addLoan to a nicer view.
   // TODO: add option to see the state of loan application.
-  
+
   const id = props.id
-  console.log("Id is: ", id)  
+  console.log("Id is: ", id)
 
   const [Dates, setDates] = useState([])
   const [Loans, setLoans] = useState(null);
   const [ShowAlert, setShowAlert] = useState(false)
   const [alertMsg, setAlertMsg] = useState("")
-  const [Error, setError] = useState(false) 
+  const [Error, setError] = useState(false)
   const [ErrorMsg, setErrorMsg] = useState("")
 
   useEffect(() => {
     const fetchLoans = async () => {
       try {
         const response = await fetch(`https://localhost:7275/api/LoanDetails/GetUserLoans/${id}`);
-        console.log("fetch is in: ", `https://localhost:7275/api/LoanDetails/GetUserLoans/${id}`)
         if (response == null || response.length === 0) {
+          console.log("Is alert display?")
           setAlertMsg("No loans in your account.")
           setShowAlert(true)
         }
-        else{
+        else {
           const data = response.json();
+          if(data == {} || data == null || data.length == undefined){
+            setAlertMsg("No loans in your account.")
+            setShowAlert(true)
+            console.log("Server response, but no loans in account.")
+          }
+          console.log("Data  is: ",data.length)
           console.log("Server responsed!! Data: " + JSON.stringify(data));
 
         }
-        
-             }
+
+      }
       catch (error) {
         console.log("Error: " + error);
         setErrorMsg(error);
@@ -84,19 +90,19 @@ export default function Loans(props) {
         Loans
       </h2>
       {Loans === null ? (
-        console.log("Loans is null","ShowAlert is: ",ShowAlert)) :  
-                Loans.length === 0 ? (
-          console.log("Loans is empty","ShowAlert is: ",ShowAlert)) :
-           Loans != null ? (
-          console.log("Loans is not empty", "ShowAlert is: ",ShowAlert),
-          <div>{LoansInfo}</div>
-        ) : <></>
+        console.log("Loans is null", "ShowAlert is: ", ShowAlert)) :
+        Loans.length === 0 ? (
+          console.log("Loans is empty", "ShowAlert is: ", ShowAlert)) :
+          Loans != null ? (
+            console.log("Loans is not empty", "ShowAlert is: ", ShowAlert),
+            <div>{LoansInfo}</div>
+          ) : <></>
       }
 
       {ShowAlert ? <Alert type="info" msg="No loans in your account." /> : null}
       {Error ? <Alert type="error" msg={ErrorMsg} /> : null}
 
-      <p>Need a loan? click <a href="/AddLoan">here.</a></p>
+      <p>Need a loan? click <a href="/AddLoan/">here.</a></p>
     </div>
   )
 }
