916ac1f7743d5dab0a27b3d4311ccbe0c9af987e
SaraDaum <sara.daum.il@users.noreply.github.com>
2024-01-01 12:51:00 +0200

    More...

--- gmach_front/src/Components/JavaScriptFiles/HelpingComponent/DataTable.js
+++ gmach_front/src/Components/JavaScriptFiles/HelpingComponent/DataTable.js
@@ -81,9 +81,9 @@ export default function DataTable(props) {
       else {
         console.log("No match 😪")
       }
-    
+
     });
-      
+
     setOpen(true);
   };
   const handleClose = () => {
@@ -207,20 +207,18 @@ export default function DataTable(props) {
                             <p>Loan id: {currentRequest[0]}</p>
                             <p>Sum: {currentRequest[1]}</p>
                             <p>Return date: {currentRequest[2]}</p>
-                            <p>Deed of guarantee:
-                              {moreDetails.loanFile}
-                              <img src={`${moreDetails.loanFile}`} alt="Deed of guarantee"  style={{height:"20%", width:"20%"}}/>
+                            <p>{moreDetails.loanFile ? "Deed of guarantee: ✔️" : "Deed of guarantee: ❌"}
+                              <img src={`${moreDetails.loanFile}`} alt="Deed of guarantee unsupported." style={{ height: "20%", width: "20%" }} />
                             </p>
-                            <p>Guarantors: {moreDetails.guarantors.map((guarantor, index) => {
+                            <p>Guarantors: {moreDetails.guarantors && moreDetails.guarantors.map((guarantor, index) => {
                               return (
                                 <p key={index}>
                                   {guarantor.guarantorId}
                                   {guarantor.check ? "Check: ✔️" : "Check: ❌"}
-                                  <img src={`${guarantor.check}`} alt="Check" style={{ height: "20%", width: "20%" }} onClick={(event) => {
+                                  <img src={`${guarantor.check}`} alt="Check unsupported " style={{ height: "20%", width: "20%" }} onClick={(event) => {
                                     event.target.style.height = "100%";
                                     event.target.style.width = "100%";
                                   }} />
-                                  
                                 </p>
                               )
                             })}</p>
