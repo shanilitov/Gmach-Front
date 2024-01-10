41b8ea935f38e8da582e6d0cb5ead775daa15df4
SaraDaum <sara.daum.il@users.noreply.github.com>
2024-01-10 13:53:38 +0200

    Add message contant.

--- gmach_front/src/Components/JavaScriptFiles/HelpingComponent/Messages.js
+++ gmach_front/src/Components/JavaScriptFiles/HelpingComponent/Messages.js
@@ -6,27 +6,24 @@ import Grid from '@mui/material/Grid';
 import { List } from '@mui/material';
 import DemoPaper from '@mui/material/Paper';
 import ForumIcon from '@mui/icons-material/Forum';
+import SnackbarContent from '@mui/material/SnackbarContent';
+import "../../../CSSFiles/StylePage.css";
 
 
 
 export default function Messages(props) {
     return (
-        <div>
-            <div style={{ color: 'rgb(0, 32, 96)', }}>
+     
+            <div style={{ color: 'rgb(0, 32, 96)', width: '300%', '@media (max-width: 600px)': { width: '100%' } }}>
                 <h3>Messages from users:</h3>
-                <List>
-                    <div>
-                        <ul>
-                            <li>                            <ForumIcon />
-                            </li>
-                        </ul>
-                        <DemoPaper variant="elevation" elevation={24} style={{ padding: "5%" }}>
-                            <div>
-                                <p>Message number 1</p>
-                            </div></DemoPaper>
+                    <div className='adminMessages'>
+                    <SnackbarContent message="Admin messaged to user." sx={{width:"50%", backgroundColor:"rgb(0, 32, 96)", marginTop:"2%", marginBottom:"2%"}}/>
+                   </div>
+                    <div className='userMessages'>
+                    <SnackbarContent message="User messaged to admin. Want to get a answer. " sx={{width:"50%", backgroundColor:"rgba(223, 221, 53)",  marginTop:"2%", marginBottom:"2%", color:"rgb(0, 32, 96)"}}/>
+
                     </div>
-                </List>
             </div>
-        </div>
+       
     )
 }
