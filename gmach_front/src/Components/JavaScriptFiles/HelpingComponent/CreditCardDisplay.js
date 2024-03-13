cbb8822edc11841f08ba4dc6c71d1f4dd406c8e9
shani litoff <shanilitov@gmail.com>
2024-03-13 18:00:16 +0200

    fix the cerdit card display

--- gmach_front/src/Components/JavaScriptFiles/HelpingComponent/CreditCardDisplay.js
+++ gmach_front/src/Components/JavaScriptFiles/HelpingComponent/CreditCardDisplay.js
@@ -10,8 +10,8 @@ const Item = styled(Paper)(({ theme, selected }) => ({
     ...theme.typography.body2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    height: 60,
-    padding: theme.spacing(1),
+    // height: auto,
+    padding: theme.spacing(0.01),
     lineHeight: '60px',
     transition: 'box-shadow 0.3s ease-in-out',
     backgroundColor: selected ? '#1976d234' : 'inherit', // Set background color based on selectedCard state
@@ -22,19 +22,23 @@ const Item = styled(Paper)(({ theme, selected }) => ({
 
 
 export default function CreditCardDisplay(props) {
-    const userID = props.userID;
-    const creditCards = props.numbers
-    console.log("Type of creditCards: ", typeof creditCards)
+
+    const user = props.userID;
+    console.log("user id is:", user)
+    const userID = user.userId
+
     const [hover, setHover] = React.useState(false);
-    console.log("CreditCardDisplay: ", creditCards);
+
     const [elevation, setElevation] = React.useState(3);
     const [selectedCard, setSelectedCard] = React.useState(null); // Add selectedCard state
     const [showAlert, setShowAlert] = useState(false);
     const [alertMsg, setAlertMsg] = useState(false);
     const [error, setError] = React.useState(false);
     const [errorMsg, setErrorMsg] = React.useState("");
+
     const [userCards, setUserCards] = useState([]); // Array of user credit cards
     const [HasCards, setHasCard] = useState(false)
+
     const token = localStorage.getItem('token')
     let x = 0;
 
@@ -43,22 +47,24 @@ export default function CreditCardDisplay(props) {
             const response = await fetch(`https://localhost:7275/api/Card/GetAllCards/${userID}`, {
                 method: 'GET',
                 headers: {
-                  'Authorization': `Bearer ${token}`
+                    'Authorization': `Bearer ${token}`
                 }
-              });
-            const data = await response.json().then(data => {console.log(data)
-            console.log("Data is: ", data);
-            
-            if (data.length > 0) {
+            });
+            const data = await response.json().then(data => {
                 console.log(data)
-                setUserCards(data);
-                console.log("In the function")
-                setHasCard(true)
-            }
-            else {
-                setAlertMsg("You don't have any credit cards saved in system.\n Please add a new credit card.")
-                setShowAlert(true);
-            }});
+                console.log("Data is: ", data);
+
+                if (data.length > 0) {
+                    console.log(data)
+                    setUserCards(data);
+                    console.log("In the function")
+                    setHasCard(true)
+                }
+                else {
+                    setAlertMsg("You don't have any credit cards saved in system.\n Please add a new credit card.")
+                    setShowAlert(true);
+                }
+            });
         } catch (error) {
             console.error('Error fetching credit cards:', error);
             setErrorMsg("Error fetching credit cards: " + error);
@@ -81,22 +87,36 @@ export default function CreditCardDisplay(props) {
 
             {showAlert ? (<Alert severity="info" type="info" msg={alertMsg} />) : (<></>)}
             {console.log("In the main return value", userCards)}
-            {HasCards ? 
-            
-            creditCards.map(element => (
-                <div
-                    style={{ cursor: 'pointer', margin: '10px' }}
-                    onClick={() => {
-                        setSelectedCard(element);
-                        props.setCard(element);
-                    }}
-                    key={element}
-                >
-                    <Item elevation={elevation} selected={selectedCard === element}>
-                        {`XXXX-XXXX-XXXX-${element.slice(-4)}`}
-                    </Item>
-                </div>
-            )) : 'No cards found.'}
+            {HasCards ?
+
+                userCards.map(element => (
+                    <div
+                        style={{ cursor: 'pointer', margin: '10px' }}
+                        onClick={() => {
+                            setSelectedCard(element);
+                            props.setCard(element);
+                        }}
+                        key={element.cardId}
+                    >
+                        <Item elevation={elevation} selected={selectedCard === element}>
+                            {console.log('element is: ', element)}
+                            <div style={{
+                                margin:"0px",
+                            }}>
+                                {`XXXX-XXXX-XXXX-${element.creditCardNumber.slice(-4)}`}
+                              
+                                
+                            </div>
+                            <div>
+                            {`${element.ownersName} `}
+                            </div>
+                            <div>
+                            {`\n${element.cvv}                           \t${element.validity}`}
+                            </div>
+
+                        </Item>
+                    </div>
+                )) : 'No cards found.'}
 
         </Grid>
     );
